0203168ad4de36eca46fc886168ea387563d0333
Nguyen Gia Minh <154106320+giaminh1501@users.noreply.github.com>
2023-12-21 15:21:05 +0700

    Add files via upload

---
+++ lab01/cdrates.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Lab 01</title>
+	<meta charset="utf-8" >
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+</head>
+
+<body>
+    <h1>Forestville Credit Union</h1>
+    <h2>CD Rates</h2>
+    <?php
+        echo    "<ul>
+                <li>4.35% (36-Month Term CD)</li>
+                <li>3.85% (12-Month Term CD)</li>
+                <li>2.65% (6-Month Term CD)</li>
+                </ul>";
+        echo    "<p>$1,000 minimum deposit</p>
+                <br/>";
+        // /*06 - Sep - 2023 / Nguyen Gia Minh*/
+    ?>
+</body>
+</html>
